7134e0b18d16a3b47029d84acba79ffffa71e276
Alwin Maier <alwin.maier@stud.uni-goettingen.de>
2014-01-31 17:42:35 +0100

    Added consistency check for chucky.conf

--- chucky/neighborhood-discovery.sh
+++ chucky/neighborhood-discovery.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 source chucky/chucky.conf
+check_config
 
 if [ ! -d "$BAG_OF_WORDS_DIR" ]; then
-    printf 'No embedding found at %s.\n' "$BAG_OF_WORDS_DIR"
-    printf 'Run chucky/symbol-embedding.sh first.\n'
-    printf 'Exiting now.\n'
-    exit 1
+   	printf 'No embedding found at %s.\n' "$BAG_OF_WORDS_DIR"
+	printf 'Run chucky/symbol-embedding.sh first.\n'
+	printf 'Exiting now.\n'
+	exit 1
 fi
 
 if [ -f "$NEIGHBORS_FILE" ]; then
-    printf 'Neighbor file already exists.\n'
-    printf 'Exiting now.\n'
-    exit 1
+	printf 'Neighbor file already exists.\n'
+	printf 'Exiting now.\n'
+	exit 1
 fi
 
 #
