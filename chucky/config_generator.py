2bf2a1fb5b3e51c79df093d5af2582fb56f32d9b
Alwin Maier <alwin.maier@stud.uni-goettingen.de>
2014-04-01 17:01:19 +0200

    Introduced new indentifier types

--- chucky/config_generator.py
+++ chucky/config_generator.py
@@ -4,29 +4,72 @@ import logging
 
 class ConfigRecord():
     
-    def __init__(self, function, symbol, n):
+    def __init__(self, function, target_name, target_type, n_neighbors):
         self.function = function
-        self.symbol = symbol
-        self.n = n
+        self.target_name = target_name
+        self.target_type = target_type
+        self.n_neighbors = n_neighbors
         self.logger = logging.getLogger('chucky')
 
     def __str__(self):
-        s = '{} ({}) - {} ({})'
+        s = '{} ({}) - {} [{}]'
         s = s.format(
                 self.function,
                 self.function.node_id,
-                self.symbol,
-                self.symbol.node_id)
+                self.target_name,
+                self.target_type)
         return s
 
-    @staticmethod
-    def generate(identifier, identifier_type, n):
-        if identifier_type == 'symbol':
-            symbols = Symbol.find_symbols_by_name(identifier)
-            for symbol in symbols:
-                yield ConfigRecord(symbol.function(), symbol, n)
-        elif identifier_type == 'function':
-            functions = Function.find_functions_by_name(identifier)
+class ConfigGenerator(object):
+
+    def __init__(self, identifier, identifier_type, n_neighbors):
+        self.identifier = identifier
+        self.identifier_type = identifier_type
+        self.n_neighbors = n_neighbors
+
+    def generate(self):
+        configurations = []
+        if self.identifier_type == 'function':
+            functions = Function.lookup_functions_by_name(self.identifier)
             for function in functions:
-                for symbol in function.symbols():
-                    yield ConfigRecord(function, symbol, n)
+                for parameter in set(map(lambda x : x.code, function.parameters())):
+                    configurations.append(
+                            ConfigRecord(function, parameter, 'Parameter', self.n_neighbors))
+                for variable in set(map(lambda x : x.code, function.variables())):
+                    configurations.append(
+                            ConfigRecord(function, variable, 'Variable', self.n_neighbors))
+                for callee in set(map(lambda x : x.code, function.callees())):
+                    configurations.append(
+                            ConfigRecord(function, callee, 'Callee', self.n_neighbors))
+        elif self.identifier_type == 'parameter':
+            functions = Function.lookup_functions_by_parameter(self.identifier)
+            for function in functions:
+                configurations.append(
+                        ConfigRecord(function, self.identifier, 'Parameter', self.n_neighbors))
+        elif self.identifier_type == 'variable':
+            functions = Function.lookup_functions_by_variable(self.identifier)
+            for function in functions:
+                configurations.append(
+                        ConfigRecord(function, self.identifier, 'Variable', self.n_neighbors))
+        elif self.identifier_type == 'callee':
+            functions = Function.lookup_functions_by_callee(self.identifier)
+            for function in functions:
+                configurations.append(
+                        ConfigRecord(function, self.identifier, 'Callee', self.n_neighbors))
+        return configurations
+
+    def group_callees_by_function_id(self, callees):
+        groups = {}
+        for callee in callees:
+            if callee.function_id not in groups:
+                groups[callee.function_id] = []
+            groups[callee.function_id].append(callee)
+        return groups.items()
+
+    def group_callees_by_code(self, callees):
+        groups = {}
+        for callee in callees:
+            if callee.code not in groups:
+                groups[callee.code] = []
+            groups[callee.code].append(callee)
+        return groups.items()
