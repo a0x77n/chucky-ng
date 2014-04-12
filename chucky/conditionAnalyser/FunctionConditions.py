d2ff97e253a6724e2b0786c0612dbbc889953d80
Alwin Maier <alwin.maier@stud.uni-goettingen.de>
2014-04-12 16:00:12 +0200

    Tainting and normalization is performed in a single step now

--- chucky/conditionAnalyser/FunctionConditions.py
+++ chucky/conditionAnalyser/FunctionConditions.py
@@ -1,5 +1,6 @@
 from conditionAnalyser.selection.ConditionSelector import ConditionSelector
 from conditionAnalyser.normalization.ConditionNormalizer import ConditionNormalizer
+from joernInterface.JoernInterface import jutils
 
 import itertools
 
@@ -18,7 +19,40 @@ class FunctionConditions:
         return int(self.obj.node_id)
     
     def getFeatures(self):
-        conditions = ConditionSelector().getRelevantConditions(self.obj, self.symbolName, self.symbolType)
-        normalizedConditions = ConditionNormalizer().normalize(conditions, self.obj, self.symbolName, self.symbolType)
-        return itertools.chain(*normalizedConditions)
-        
+        #conditions = ConditionSelector().getRelevantConditions(self.obj, self.symbolName, self.symbolType)
+        #normalizedConditions = ConditionNormalizer().normalize(conditions, self.obj, self.symbolName, self.symbolType)
+        #normalizedConditions = set(itertools.chain(*normalizedConditions))
+        #for i, feat in enumerate(sorted(normalizedConditions)):
+        #    print i, '\t', feat
+
+        normalizedConditions = self.conditions()
+        normalizedConditions = set(normalizedConditions)
+        #for i, feat in enumerate(sorted(normalizedConditions)):
+        #    print i, '\t', feat
+        #return itertools.chain(*normalizedConditions)
+        return normalizedConditions
+
+    def conditions(self):
+        lucene_query = 'conditions = []; argList = []; retList = []; queryNodeIndex(\'type:"{}" AND functionId:"{}" AND code:"{}"\')'
+        lucene_query = lucene_query.format(self.symbolType, self.obj.node_id, self.symbolName)
+        traversal = ''
+        if self.symbolType == 'Callee':
+            traversal += (
+                    'copySplit('
+                    '_().calleeToArguments().store(argList, {it.code}).taintUpwards(),'
+                    '_().calleeToReturnValue().store(retList, {it.code}).taintDownwards())'
+                    '.fairMerge().dedup()'
+            )
+        else:
+            traversal += (
+                    'copySplit('
+                    '_().taintUpwards(),'
+                    '_().taintDownwards())'
+                    '.fairMerge().dedup()'
+            )
+        traversal += '.symbolToUsingConditions().dedup()'
+        traversal += '.store(conditions).iterate()'
+        command = '.'.join([lucene_query, traversal])
+        command += '; g.transform{ conditions }.scatter().normalize(argList, retList)'
+        x = jutils.joern.runGremlinQuery(command)
+        return x
