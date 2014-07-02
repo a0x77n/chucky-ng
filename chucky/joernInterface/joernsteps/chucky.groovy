a26b74cba63048b7eed092195405634154481188
fabs <fyamagu@gwdg.de>
2014-07-02 12:31:11 +0200

    Patch for backwardSlice.

--- chucky/joernInterface/joernsteps/chucky.groovy
+++ chucky/joernInterface/joernsteps/chucky.groovy
@@ -60,13 +60,15 @@ Gremlin.defineStep('forwardSlice', [Vertex, Pipe], { symbol ->
 Gremlin.defineStep('backwardSlice', [Vertex, Pipe], { symbol ->
 	_()
 	.copySplit(
-		_(),
-		_().sideEffect{first = true}
+	_(),
+	_().transform{
+		it.sideEffect{first = true;}
 		.inE('REACHES', 'CONTROLS')
 		.filter{it.label == 'CONTROLS' || !first || it.var == symbol}
-		.outV()
+		.outV().gather{it}.scatter()
 		.sideEffect{first = false}
 		.loop(4){it.loops < 5}{true}
+	}.scatter()
 	).fairMerge()
 	.dedup()
 });
